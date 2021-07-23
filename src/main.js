b05a253fb294633ec64d2bd8a672de928ed73b73
Edgar <edgar.i.esparza@gmail.com>
2021-07-23 15:23:16 -0500

    added etsy icon and link to footer

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+  faTwitter,
+  faEtsy
+} from "@fortawesome/free-brands-svg-icons";
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+library.add(faFacebook, faInstagram, faYoutube, faTwitter, faEtsy);
 
 Vue.config.productionTip = false;
 
